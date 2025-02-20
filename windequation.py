1d2a118a1d158c18d377258756582059f92bec75
mariadryagina <maria.dryagina.7579@student.uu.se>
2025-02-20 10:36:33 +0100

    adding a pyhton file

---
+++ windequation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt 
+
+watt=np.array([40,122,290,560,980,1550,2300,3300,4500,6000,7800,7800,7800,7800])
+m_s=np.array([2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+
+
+# Creating the plot
+plt.figure(figsize=(10, 5))
+plt.plot(m_s, watt, marker='o', linestyle='-', label='Power Output')
+plt.xlabel('Wind Speed (m/s)')
+plt.ylabel('Power Output (W)')
+plt.title('Wind Speed vs. Power Output')
+plt.legend()
+plt.grid(True)
+plt.show()
--- windpower.py
+++ windpower.py
@@ -1,10 +1,10 @@
 from numpy import *
 import pandas as pd
 from math import *
+import matplotlib.pyplot as plt
  
 #Reading excel file 
-path_to_file = "/Users/a517469/Python/winddata.xlsx"
- 
+path_to_file = "/Users/a517469/Python/energy-optimization/winddata1.xlsx"
 df = pd.read_excel(path_to_file)
 
 #Creting a matrix with zeros
@@ -13,43 +13,50 @@ V=zeros((24,366))
 for i in range(366):
     for j in range(24):
          V[j,i]=df.iloc[i*24+j,4]
-	
+
+
+#If a value is greater than 12 it will be changed to 12	
 for i in range(24):
     for j in range(366):
         if V[i, j] > 12:
             V[i, j] = 12
 
-#V_df = pd.DataFrame(V)
-#print(V_df)
 
-# Calculate the mean value of the entire matrix V
-mean_value = mean(V)
-print(f"The mean value of the matrix V: {mean_value}")
-
-
-
+#_Function begins_______________________________________________________________#
 #V - wind speed in m/s
 #n - amount of turbines
 #The formula for wind power is P_w=4,8653*V^2,9637 and it is in W
 
 def windpower(V,n):
      
-     P_wind=(4.8653*V**2.9637)*n/1000
+     P_wind=(4.8653*V**2.9637*n)/(1000*1000)
 
      return P_wind
+#_______________________________________________________________________________#
 
+#Calling on function
 P_wind1=windpower(V,1)
-P_wind2=pd.DataFrame(P_wind1)
-print(P_wind2)
 
-V_1=zeros((366))
+#Creating an array
+P_wind2=zeros((366))
 
+#Storing the sum of each column in the array
 for i in range(366):
-      V_1[i]=sum(P_wind1[:,i])
+      P_wind2[i]=sum(P_wind1[:,i])
 
-print(V_1)
+#Calculating the sum of the array
+total_sum=ceil(sum(P_wind2)*10)/10
+print(f"The yearly production of wind power 2024 is {total_sum}MWh")
 
-total_sum=sum(V_1)
-print(total_sum)
+#_______________________________________________________________________________#
+# Creating a plot
+plt.figure(figsize=(10, 5))
+plt.plot(range(1, 367), P_wind2*1000, label='Daily Wind Power Production')
+plt.xlabel('Day of the Year')
+plt.ylabel('Wind Power Production (kWh)')
+plt.title('Daily Wind Power Production in 2024')
+plt.legend()
+plt.grid(True)
+plt.show()
 
 

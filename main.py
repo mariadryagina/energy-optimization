8ca3fc3a8773a9883e042f89557e468622eacbe9
majaenestrom <maja.enestrom.4859@student.uu.se>
2025-03-11 14:20:44 +0100

    main script

---
+++ main.py
@@ -0,0 +1,29 @@
+# Reference case
+
+import numpy as np
+import matplotlib.pyplot as plt
+import load_krossholmen_2023
+import load_krossholmen_2024
+import load_björkö_bessekroken
+import el_price
+
+Load_2023 = load_krossholmen_2023.load_aug
+print("Load August 2023",Load_2023)
+
+Load_2024 = load_krossholmen_2024.load_aug
+print("Load August 2024",Load_2024)
+
+Load_2024_b = load_björkö_bessekroken.load_aug
+
+spotprice=el_price.spotprice_2024[:, 213:244]
+
+electricity_price= Load_2024_b * spotprice 
+print("Spotprice August 2024", electricity_price)
+
+yearly_electricityprice = electricity_price.sum(axis=0)
+print("Total:", yearly_electricityprice.sum(axis=0))
+
+plt.figure(figsize=(10, 5))
+plt.plot(yearly_electricityprice)
+plt.grid(True)
+plt.show()

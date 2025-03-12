c3b3d1cb172539b7316fa88265f51cb98c8ad4f7
majaenestrom <maja.enestrom.4859@student.uu.se>
2025-03-12 09:43:06 +0100

    elprices

--- el_price.py
+++ el_price.py
@@ -1,31 +1,45 @@
 import pandas as pd
 import numpy as np
 
-path_to_file = "/Users/a518244/Python/energy-optimization/measurePointConsumptionPrice.xlsx"
+path_to_file4 = "/Users/a518244/Python/energy-optimization/Spotpriser/price_2024.csv"
+path_to_file3 = "/Users/a518244/Python/energy-optimization/Spotpriser/price_2023.csv"
 
-df = pd.read_excel(path_to_file)
+# Read the CSV file
+df4 = pd.read_csv(path_to_file4)
+df3 = pd.read_csv(path_to_file3)
 
-# Extract values from the fourth column third row-2185th row (index 3)
-spotprice_values = df.iloc[1:2184, 3].values
+# Extract values from the second column (index 1)
+values_2023 = df3.iloc[0:8761, 1].values
+values_2024 = df4.iloc[0:8785, 1].values
 
 # Create a matrix (reshape if needed, here assuming a 1D array)
-spotprice_matrix = np.array(spotprice_values)
-
-print(spotprice_matrix)
+spotprice_matrix_2023 = np.array(values_2023)
+spotprice_matrix_2024 = np.array(values_2024)
 
 # Initialize an empty 2D array with appropriate dimensions
 rows = 24
-cols = -(-len(spotprice_matrix) // rows)  # Calculate number of columns needed, rounding up
-spotprice = np.zeros((rows, cols))
+cols_2023 = -(-len(spotprice_matrix_2023) // rows)  # Calculate number of columns needed, rounding up
+spotprice_2023 = np.zeros((rows, cols_2023))
 
-# Fill the matrix "spotprice" with values
-for i, value in enumerate(spotprice_matrix):
+# Fill the matrix "spotprice_matrix_2023" with values
+for i, value in enumerate(spotprice_matrix_2023):
     row = i % rows
     col = i // rows
-    spotprice[row, col] = value
+    spotprice_2023[row, col] = value
 
-#print(spotprice)
+rows = 24
+cols_2024 = -(-len(spotprice_matrix_2024) // rows)  # Calculate number of columns needed, rounding up
+spotprice_2024 = np.zeros((rows, cols_2024))
 
-spotprice_df = pd.DataFrame(spotprice)
-print(spotprice_df)
+# Fill the matrix "spotprice_matrix_2024" with values
+for i, value in enumerate(spotprice_matrix_2024):
+    row = i % rows
+    col = i // rows
+    spotprice_2024[row, col] = value
+
+# Printing the spotprice
+# spotprice_df_2023 = pd.DataFrame(spotprice_matrix_2023)
+# spotprice_df_2024 = pd.DataFrame(spotprice_matrix_2024)
+# print(spotprice_df_2023)
+# print(spotprice_df_2024)
 

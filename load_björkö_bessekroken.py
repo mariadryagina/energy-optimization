3566cd8cb9337b171829de873bc3c2bf7e337def
mariadryagina <maria.dryagina.7579@student.uu.se>
2025-03-13 10:36:48 +0100

    inte ändrat något

--- load_björkö_bessekroken.py
+++ load_björkö_bessekroken.py
@@ -25,6 +25,12 @@ load_df = pd.DataFrame(np.transpose(load_values))
 
 load=np.transpose(load_values)
 
+# Convert the NumPy array to a DataFrame
+load_df1 = pd.DataFrame(load)
+
+# Save the frequency data to a CSV file for further analysis
+load_df1.to_csv('load_björkö_bessekroken', index=False)
+
 yearly_load = load.sum(axis=0)
 total = yearly_load.sum()/1000
 print("*The total load for full year is", round(total, 1), "MWh")

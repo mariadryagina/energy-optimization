af4d665975e077b9efca8766b8138668838130a4
mariadryagina <maria.dryagina.7579@student.uu.se>
2025-03-17 11:44:25 +0100

    fixat massa kul

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 #______Importing libraries_______________________________________________________________________________________________
+#region
 import numpy as np
 import matplotlib.pyplot as plt
 import load_krossholmen_2023
 import solarpower
 import windpower
-#import load_krossholmen_2024
+import load_björkö
 import load_björkö_bessekroken
 import el_price
-
-#______Variables____________________________________________________________________________________________________________
-#Load_2023 = load_krossholmen_2023.load_aug
-#yearly_load = load_krossholmen_2023.yearly_load
-#yearly_load_bessekroken = load_björkö_bessekroken.yearly_load
-load_bessekroken=load_björkö_bessekroken.load
-
-#________Reference Case____________________________________________________________________________________________________
-#Calculating the sum of the array for the yearly load 1X365 at Besskroken
+#endregion
+#______Variable and parameters___________________________________________________________________________________________
+#region
+load_krossholmen = load_krossholmen_2023.load
+load_björkö_hamn=load_björkö.load
+load_bessekroken=load_björkö_bessekroken.load 
+load_winter_krossholmen = load_krossholmen_2023.load_winter
+#endregion
+#________Reference Case__________________________________________________________________________________________________
+#region Calculating the sum of the array for the yearly load 1X365 in Besskroken
 yearly_load_bessekroken=np.zeros((365))
 for i in range(365):
     yearly_load_bessekroken[i] = sum(load_bessekroken[:, i])
 
-#_________Plotting_________________________________________________________________________________________________________
-#Plotting the daily load at Bessekroken
+#Calculating the sum of the array for the yearly load 1X365 in Krossholmen
+yearly_load_krossholmen=np.zeros((365))
+for i in range(365):
+    yearly_load_krossholmen[i] = sum(load_krossholmen[:, i])
+
+#Calculating the sum of the array for the yearly load 1X365 in Krossholmen
+yearly_load_björkö=np.zeros((365))
+for i in range(365):
+    yearly_load_björkö[i] = sum(load_björkö_hamn[:, i])
+#endregion
+#_________Plotting Reference Case________________________________________________________________________________________
+#region Plotting the daily load in Bessekroken
 plt.figure(figsize=(10, 5))
 plt.plot(range(len(yearly_load_bessekroken)), yearly_load_bessekroken, label='Electricity Load')
 plt.fill_between(range(len(yearly_load_bessekroken)), yearly_load_bessekroken, alpha=0.3)
-plt.xlabel('Days of the Year')
+plt.xlabel('Days')
 plt.ylabel('Daily Load (kWh)')
-plt.title('Electricity Load in 2023')
+plt.title('Electricity Load in 2023 in Bessekroken')
 #plt.legend()
 plt.xlim(0, 364)  # Set x-axis limits to start at 0 and end at 364
 plt.ylim(0, (max(yearly_load_bessekroken)+5)) 
@@ -35,8 +47,55 @@ plt.ylim(0, (max(yearly_load_bessekroken)+5))
 plt.show(block=False)
 plt.show()
 
-#_________Case 1_______________________________________________________________________________________________________________
-#Calculating selfproduced electricity
+
+#Plotting the daily load in Krossholmen
+plt.figure(figsize=(10, 5))
+plt.plot(range(len(yearly_load_krossholmen)), yearly_load_krossholmen, label='Electricity Load')
+plt.fill_between(range(len(yearly_load_krossholmen)), yearly_load_krossholmen, alpha=0.3)
+plt.xlabel('Days')
+plt.ylabel('Daily Load (kWh)')
+plt.title('Electricity Load in 2023 in Krossholmen')
+#plt.legend()
+plt.xlim(0, 364)  # Set x-axis limits to start at 0 and end at 364
+plt.ylim(0, (max(yearly_load_krossholmen)+100)) 
+#plt.grid(True)
+plt.show(block=False)
+plt.show()
+
+# Create subplots
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+fig.suptitle('Mean FCR-D up Prices During Different Seasons')
+# Plot mean winter daily load in Krossholmen
+axs[0].plot(sum(load_winter_krossholmen)/len(load_winter_krossholmen))
+axs[0].set_title('Winter')
+axs[0].set_ylabel('SEK/MWh')
+axs[0].set_xlabel('Hour of the day')
+axs[0].set_xlim(0, 23)
+axs[0].grid(True)
+# Plot mean summer daily load in Krossholmen
+axs[1].plot(price_summer_mean_FCR_D_up)
+axs[1].set_title('Summer')
+axs[1].set_ylabel('SEK/MWh')
+axs[1].set_xlabel('Hour of the day')
+axs[1].set_xlim(0, 23)
+axs[1].grid(True)
+
+#Plotting the daily load in Björkö marina
+plt.figure(figsize=(10, 5))
+plt.plot(range(len(yearly_load_björkö)), yearly_load_björkö, label='Electricity Load')
+plt.fill_between(range(len(yearly_load_björkö)), yearly_load_björkö, alpha=0.3)
+plt.xlabel('Days of the Year')
+plt.ylabel('Daily Load (kWh)')
+plt.title('Electricity Load in 2023 in Björkö')
+#plt.legend()
+plt.xlim(0, 364)  # Set x-axis limits to start at 0 and end at 364
+plt.ylim(0, (max(yearly_load_björkö)+100)) 
+#plt.grid(True)
+plt.show(block=False)
+plt.show()
+#endregion
+#_________Case 1_________________________________________________________________________________________________________
+#region Calculating selfproduced electricity
 #Calling on function, change the values to get the desired result
 
 P_s2=solarpower.solar(167,0.20) #167m^2, 20% efficiency, which is the values of Krossholmen
@@ -72,11 +131,9 @@ print(f"The yearly production of solar power is {total_sum_sun/1000}MWh")
 
 total_sum_wind=round(sum(P_wind3)*10)/10
 print(f"The yearly production of wind power is {total_sum_wind/1000}MWh")
-
-
-
-#______Plotting_____________________________________________________________________________________________________________
-# Plotting the daily electricity load
+#endregion
+#______Plotting Case 1___________________________________________________________________________________________________
+#region Plotting the daily electricity load
 plt.figure(figsize=(10, 5))
 #plt.plot(range(len(P_s3)), P_s3, label='Solar Power Production')
 plt.plot(range(len(yearly_load_bessekroken)), yearly_load_bessekroken, label='The load of Bessekroken')
@@ -108,37 +165,4 @@ plt.ylim(0, (max(max(P_wind3), max(P_s3))+5))
 #plt.grid(True)
 plt.show(block=False)
 plt.show()
-
-
-
-
-#______Maja's stuff__________________________________________________________________________________________________________
-# Load_2024 = load_krossholmen_2024.load_aug
-# print("Load August 2024",Load_2024)
-
-#Load_2024_b = load_björkö_bessekroken.load_aug
-
-# spotprice=el_price.spotprice_2024[:, 213:244]
-
-# electricity_price= Load_2023 * spotprice 
-# print("Spotprice August 2023", electricity_price)
-
-# yearly_electricityprice = electricity_price.sum(axis=0)
-# print("Total:", yearly_electricityprice.sum(axis=0))
-
-# plt.figure(figsize=(10, 5))
-# plt.plot(yearly_electricityprice)
-# plt.grid(True)
-# plt.show()
-
-# #Plotting the daily load in krossholmen
-# plt.figure(figsize=(10, 5))
-# plt.plot(range(len(yearly_load)), yearly_load, label='Daily Electricity Load')
-# plt.fill_between(range(len(yearly_load)), yearly_load, alpha=0.3, label='Daily Load Area')
-# plt.xlabel('Day of the Year')
-# plt.ylabel('Daily Load (kWh)')
-# plt.title('Daily Electricity Load in 2023')
-# plt.legend()
-# plt.grid(True)
-# plt.show(block=False)
-# plt.show()
+#endregion

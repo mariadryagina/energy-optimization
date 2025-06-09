ad17b3b96948804e261e65f6193920ac161f8fce
majaenestrom <maja.enestrom.4859@student.uu.se>
2025-06-09 13:19:53 +0200

    Update el_cost.py

--- el_cost.py
+++ el_cost.py
@@ -32,16 +32,5 @@ def cost(P_sun, P_wind, P_load, power_tariff, tax_cost, transmission_cost, VAT):
     peak_month = np.array([np.max(P_load[:, 0:31]), np.max(P_load[:, 31:59]), np.max(P_load[:, 59:90]), np.max(P_load[:, 90:120]), np.max(P_load[:, 120:151]), np.max(P_load[:, 151:181]), np.max(P_load[:, 181:212]), np.max(P_load[:, 212:243]), np.max(P_load[:, 243:273]), np.max(P_load[:, 273:304]), np.max(P_load[:, 304:334]), np.max(P_load[:, 334:365])])
     for i in range(12):
         peak_cost[i] = peak_month[i] * power_tariff
-    #peak_cost_matrix = np.zeroes((24,365))
-    # Deviding the fixed costs for each hour
-    # for i in range(12):
-    #     if i == 0:
-    #         peak_cost_matrix [:, 0:31] = peak_cost[i]/(len(P_load[:, 0:31])*24)
-                
-    #Calculating the total cost for the year
-    #total_cost_matrix = np.zeroes((24,365))
-    #total_cost_matrix = (cost_electricity + cost_tax + cost_transmission)*VAT
-    # print(total_cost_matrix)
-    # print(total_cost_matrix.shape)
     total_cost = (cost_electricity.sum() + cost_tax.sum() + cost_transmission.sum() + peak_cost.sum())*VAT 
     return total_cost

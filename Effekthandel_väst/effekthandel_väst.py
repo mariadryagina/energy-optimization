1f608c6bd0cbb8055138a718622a6919b2a0a420
mariadryagina <maria.dryagina.7579@student.uu.se>
2025-06-09 13:06:05 +0200

    Update effekthandel_väst.py

--- Effekthandel_väst/effekthandel_väst.py
+++ Effekthandel_väst/effekthandel_väst.py
@@ -59,13 +59,6 @@ for day in range(365):
                     # Set bids for hours 17-19
                     I_bid[17:19, day + offset] = 1
 
-# # Print the bid matrix to verify
-# print(I_bid)
-# # Count the total number of 1s in the I_bid matrix
-# total_bids = I_bid.sum()
-
-# # Print the result
-# print(f"Total number of bids (1s) in I_bid: {total_bids}")
 
 #Creating a matrix for when the bids are activated
 # Initialize the activation matrix (same shape as I_bid)
@@ -80,29 +73,6 @@ for day in range(365):
         blocks_with_bids.append((day, range(17, 19)))  # Add the 17-19 block
 
 
-
-# # Define specific positions to activate (indices of blocks_with_bids)
-# specific_positions = [10,40, 50,60, 90, 100,120]  # Replace with the specific indices you want to activate
-# # print(f"Total number of blocks with bids: {len(blocks_with_bids)}")
-
-# # Ensure the number of positions matches 5% of the total blocks
-# num_blocks_to_activate = int(len(blocks_with_bids) * 0.05)
-# if len(specific_positions) != num_blocks_to_activate:
-#     raise ValueError("The number of specific positions must match 5 the total blocks.")
-
-
-
-# # Activate the selected blocks
-# for block_index in specific_positions:
-#     day, block = blocks_with_bids[block_index]  # Get the day and block
-#     for hour in block:
-#         I_activated[hour, day] = 1  # Activate the entire block
-
-# # Print the activation matrix to verify
-# #print(I_activated)
-
-
-
 # Randomly select 13% of these blocks for activation
 #region
 num_blocks_to_activate = int(len(blocks_with_bids) * 0.13)  # 13% of the blocks
@@ -114,62 +84,6 @@ for block_index in activated_blocks:
     for hour in block:
         I_activated[hour, day] = 1  # Activate the entire block
 
-# # Print the activation matrix to verify
-# print(I_activated)
-
-# print(f"Total activated bids: {I_activated.sum()}")  # Each block has 3 hours
-# print(f"Activated blocks: {len(activated_blocks)}")
-# #endregion
 # #endregion
 
 
-
-
-#____Function_______________________________________________________________________________________
-#region
-
-R_cap= 200/1000 #0,2 SEK/kWh
-R_energy = 3500/1000 #3,5 SEK/kWh
-boat_capacity=100 
-bess_capacity=533
-number_boats=3
-range=0.8 
-
-def flexibility_market(R_cap, R_energy, boat_capacity, bess_capacity, number_boats, range):
-    P_bid=(((number_boats*boat_capacity)+bess_capacity)*range)/2
-    R_LFM_cap=I_bid*R_cap*P_bid
-    R_LFM_activated=I_activated*R_energy*P_bid
-    R_LFM=R_LFM_cap+R_LFM_activated
-    
-    return R_LFM, P_bid
-
-R_LFM, P_bid =flexibility_market(R_cap, R_energy, boat_capacity, bess_capacity, number_boats, range)
-
-# print(R_LFM)
-# print(P_bid)
-
-# total_revenue=R_LFM.sum()
-
-# # Print the result
-# print(f"Total revenue generated: {total_revenue}")
-
-#endregion
-
-#____CSV files____________________________________________________________________________________
-#region
-
-# # Optionally, save the activation matrix to a CSV file
-# I_activated_df = pd.DataFrame(I_activated)
-# I_activated_df.to_csv('I_activated.csv', index=False)
-
-
-# # Convert the NumPy array to a DataFrame
-# I_bid_df = pd.DataFrame(I_bid)
-# # Save the bid matrix to a CSV file for further analysis
-# I_bid_df.to_csv('I_bid.csv', index=False)
-
-# # Optionally, save the activation matrix to a CSV file
-# R_LFM_df = pd.DataFrame(R_LFM)
-# R_LFM_df.to_csv('R_LFM.csv', index=False)
-
-#endregion
